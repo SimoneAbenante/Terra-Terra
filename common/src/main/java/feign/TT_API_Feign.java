f2bade9ef0bc39c009efa2dbaba731ff1b136979
SimoneAbenante <linkin-pak66>
2019-05-29 15:12:11 +0200

    Aggiornamento Feign

--- common/src/main/java/feign/TT_API_Feign.java
+++ common/src/main/java/feign/TT_API_Feign.java
@@ -22,85 +22,85 @@ public interface TT_API_Feign {
 
 //Bill Controller
 
-	@GetMapping(value = "/", produces = "application/json")
+	@GetMapping(value = "/bills/", produces = "application/json")
 	public List<BillDto> getAllBill();
 
-	@GetMapping(value = "/{id}", produces = "application/json")
+	@GetMapping(value = "/bills/{id}", produces = "application/json")
 	public BillDto getBillById(@PathVariable Integer id);
 
-	@PostMapping(value = "/", produces = "application/json")
+	@PostMapping(value = "/bills/", produces = "application/json")
 	public BillDto saveBill(@RequestBody BillDto dto);
 
-	@DeleteMapping(value = "/{id}", produces = "application/json")
+	@DeleteMapping(value = "/bills/{id}", produces = "application/json")
 	public Boolean deleteBill(@PathVariable Integer id);
 
 //Dish Controller
 
-	@GetMapping(value = "/", produces = "application/json")
+	@GetMapping(value = "/dishes/", produces = "application/json")
 	public List<DishDto> getAllDish();
 	
-	@GetMapping(value = "/{id}", produces = "application/json")
+	@GetMapping(value = "/dishes/{id}", produces = "application/json")
 	public DishDto getDishById(@PathVariable Integer id);
 	
-	@PostMapping(value = "/", produces = "application/json")
+	@PostMapping(value = "/dishes/", produces = "application/json")
 	public DishDto saveDish(@RequestBody DishDto dto);
 	
-	@DeleteMapping(value = "/{id}", produces = "application/json")
+	@DeleteMapping(value = "/dishes/{id}", produces = "application/json")
 	public Boolean deleteDish(@PathVariable Integer id);
 	
 //DiningTable Controller
 	
-	@GetMapping(value = "/", produces = "application/json")
+	@GetMapping(value = "/tables/", produces = "application/json")
 	public List<DiningTableDto> getAllDiningTable();
 	
-	@GetMapping(value = "/{id}", produces = "application/json")
+	@GetMapping(value = "/tables/{id}", produces = "application/json")
 	public DiningTableDto getDiningTableById(@PathVariable Integer id);
 	
-	@PostMapping(value = "/", produces = "application/json")
+	@PostMapping(value = "/tables/", produces = "application/json")
 	public DiningTableDto saveDiningTable(@RequestBody DiningTableDto dto);
 	
-	@DeleteMapping(value = "/{id}", produces = "application/json")
+	@DeleteMapping(value = "/tables/{id}", produces = "application/json")
 	public Boolean deleteDiningTable(@PathVariable Integer id);
 	
-	@PostMapping(value = "/status", produces = "application/json")
+	@PostMapping(value = "/tables/status", produces = "application/json")
 	public Boolean setAllDiningTableStatus(@RequestParam Integer idStatus);
 	
-	@PostMapping(value = "/{id}/status/", produces = "application/json")
+	@PostMapping(value = "/tables/{id}/status/", produces = "application/json")
 	public Boolean setDiningTableStatus(@PathVariable Integer idTable, @RequestParam Integer idStatus);
 	
 //Job Controller
 	
-	@GetMapping(value = "/", produces = "application/json")
+	@GetMapping(value = "/jobs/", produces = "application/json")
 	public List<JobDto> getAllJob();
 
-	@GetMapping(value = "/{id}", produces = "application/json")
+	@GetMapping(value = "/jobs/{id}", produces = "application/json")
 	public JobDto getJobById(@PathVariable Integer id);
 
-	@GetMapping(value = "/bill", produces = "application/json")
+	@GetMapping(value = "/jobs/bill", produces = "application/json")
 	public List<JobDto> getAllJobByBillId(@RequestParam Integer idBill);
 
-	@PostMapping(value = "/", produces = "application/json")
+	@PostMapping(value = "/jobs/", produces = "application/json")
 	public JobDto saveJobById(@RequestBody JobDto jobDto);
 
-	@PostMapping(value = "/params", produces = "application/json")
+	@PostMapping(value = "/jobs/params", produces = "application/json")
 	public List<JobDto> saveJobListById(@RequestParam(required = false) Integer idBill,
 			@RequestBody Table_Dishes listOfBill_Dishes);
 
-	@DeleteMapping(value = "/{id}", produces = "application/json")
+	@DeleteMapping(value = "/jobs/{id}", produces = "application/json")
 	public Boolean deleteJob(@PathVariable Integer id);
 	
 //Status Controller
 	
-	@GetMapping(value = "/", produces = "application/json")
+	@GetMapping(value = "/statuses/", produces = "application/json")
 	public List<StatusDto> getAllStatus();
 	
-	@GetMapping(value = "/{id}", produces = "application/json")
+	@GetMapping(value = "/statuses/{id}", produces = "application/json")
 	public StatusDto getStatusById(@PathVariable Integer id);
 	
-	@PostMapping(value = "/", produces = "application/json")
+	@PostMapping(value = "/statuses/", produces = "application/json")
 	public StatusDto saveStatus(@RequestBody StatusDto statusDto);
 	
-	@DeleteMapping(value = "/{id}", produces = "application/json")
+	@DeleteMapping(value = "/statuses/{id}", produces = "application/json")
 	public Boolean deleteStatus(@PathVariable Integer id);
 	
 }
